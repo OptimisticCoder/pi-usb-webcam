a857f85894cb1d403db2b02d2f04842f561e3177
OptimisticCoder <danpatching@gmail.com>
2016-10-02 00:33:05 +0000

    Set up for boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
 #include <stdio.h>
+#include <iostream>
+
+using namespace cv;
 
 int main( int argc, const char* argv[] )
 {
-	printf( "\nHello World\n\n" );
+	VideoCapture cap(CV_CAP_ANY);
+	if(cap.isOpened())
+	{
+		printf( "Connected to webcam.\n" );
+		while(cap.isOpened())
+		{
+			Mat frame;
+			if(!cap.read(frame))
+				break;
+
+		}		
+
+		printf("Press enter to exit.\n");
+		std::cin.get();
+	}
+	else
+		printf( "Couldn't connect to webcam.\n" );
 }
